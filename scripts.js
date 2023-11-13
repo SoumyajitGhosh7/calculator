e46f77d0cf33a1e79dc9b534ca53c6a5d6d07be5
SoumyajitGhosh7 <150497987+SoumyajitGhosh7@users.noreply.github.com>
2023-11-13 13:41:49 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
-let str='';
+str=localStorage.getItem('ans');
+document.querySelector('#display').value=str;
+
 function dis(c){
+    str=localStorage.getItem('ans');
     if(c=='d'){
         str = str.slice(0, -1);
+        localStorage.setItem('ans',str);
     }else
     if(c=='='){
         str=(eval(str)).toString();
+        localStorage.setItem('ans',str);
     }else
     if(c=='AC'){
         str='';
+        localStorage.clear();
     }else
     str=str+c;
+    localStorage.setItem('ans',str);
     document.querySelector('#display').value=str;
     return str;
 }
