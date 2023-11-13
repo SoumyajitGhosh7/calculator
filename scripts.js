00b8aa6f2b47836a9ed54eabe59a8ce95a142f97
SoumyajitGhosh7 <150497987+SoumyajitGhosh7@users.noreply.github.com>
2023-11-13 19:29:02 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,29 @@
-str=localStorage.getItem('ans');
-if(str==null){
-    str='';
-}
-document.querySelector('#display').value=str;
 
-function dis(c){
-    str=localStorage.getItem('ans');
-    if(c=='d'){
+
+str = localStorage.getItem('ans');
+
+document.querySelector('#display').value = str;
+
+function dis(c) {
+    str = localStorage.getItem('ans');
+    if (c == 'd') {
         str = str.slice(0, -1);
-        localStorage.setItem('ans',str);
-    }else
-    if(c=='='){
-        str=(eval(str)).toString();
-        localStorage.setItem('ans',str);
-    }else
-    if(c=='AC'){
-        str='';
-        localStorage.clear();
-    }else
-    str=str+c;
-    localStorage.setItem('ans',str);
-    document.querySelector('#display').value=str;
+        localStorage.setItem('ans', str);
+    } else
+        if (c == '=') {
+            str = (eval(str)).toString();
+            localStorage.setItem('ans', str);
+        } else
+            if (c == 'AC') {
+                str = '';
+                localStorage.clear();
+            } else
+                if (str == null) {
+                    str = c;
+                } else
+                    str = str + c;
+    localStorage.setItem('ans', str);
+    document.querySelector('#display').value = str;
     return str;
 }
+
